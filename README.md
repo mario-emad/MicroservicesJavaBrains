53e631dddbb0ba9ce22fb4250258041977e51c82
Mario-Emad <marioemad95new@gmail.com>
2026-02-08 21:20:31 +0200

    Add refresh config server changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MicroservicesJavaBrains
+
+**Notes:**
+
+- To refresh the configurations server values in runtime just call **http://localhost:8101/actuator/refresh** API with adding the **@RefreshScope** annotation to the controller that have the properties which we need to update its values
--- spring-cloud-config-client/src/main/java/io/mario/springcloudconfigclient/controller/SpringCloudConfigClientResource.java
+++ spring-cloud-config-client/src/main/java/io/mario/springcloudconfigclient/controller/SpringCloudConfigClientResource.java
@@ -3,6 +3,7 @@ package io.mario.springcloudconfigclient.controller;
 import io.mario.springcloudconfigclient.model.DBModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class SpringCloudConfigClientResource {
 
 
